cf5da8c16ddf11f86262ac1d2def1623af67c4f7
blackjok3r <blackjok3r@gmail.com>
2018-10-26 18:00:40 +0800

    stremaer scripts first try

---
+++ streamer/getfile_fromchain.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python2
+import binascii
+import sys
+import re
+import os
+import requests
+import json
+
+# define function that fetchs rpc creds from .conf
+def def_credentials(chain):
+
+    #TODO: add osx/windows support for ac_dir
+    ac_dir = os.environ['HOME'] + '/.komodo'
+
+    # define config file path
+    if chain == 'KMD':
+        coin_config_file = str(ac_dir + '/komodo.conf')
+    else:
+        coin_config_file = str(ac_dir + '/' + chain + '/' + chain + '.conf')
+    #define rpc creds
+    with open(coin_config_file, 'r') as f:
+        #print("Reading config file for credentials:", coin_config_file)
+        for line in f:
+            l = line.rstrip()
+            if re.search('rpcuser', l):
+                rpcuser = l.replace('rpcuser=', '')
+            elif re.search('rpcpassword', l):
+                rpcpassword = l.replace('rpcpassword=', '')
+            elif re.search('rpcport', l):
+                rpcport = l.replace('rpcport=', '')
+    return('http://' + rpcuser + ':' + rpcpassword + '@127.0.0.1:' + rpcport)
+
+# define function that posts json data
+def post_rpc(url, payload, auth=None):
+    try:
+        r = requests.post(url, data=json.dumps(payload), auth=auth)
+        return(json.loads(r.text))
+    except Exception as e:
+        raise Exception("Couldn't connect to " + url + ": ", e)
+
+# define sendrawtransaction rpc
+def getdatafromblock_rpc(RPCURL, block):
+    sendrawpayload = {
+        "jsonrpc": "1.0",
+        "id": "python",
+        "method": "getdatafromblock",
+        "params": [block]}
+    return(post_rpc(RPCURL, sendrawpayload))
+
+# get arguments
+if len(sys.argv) > 2:
+    filename = sys.argv[1]
+    block = sys.argv[2]
+else:
+    print('please specify path to output file to and block to extract from')
+    sys.exit()
+
+# get rpc creds to be able to contact komodod
+KOMODODURL = def_credentials('TEST2')
+
+# get the data
+returnjson = getdatafromblock_rpc(KOMODODURL,block)
+
+pp.pprint(returnjson)
+
+sys.exit()
+# convert hex string back to binary data
+dataout = binascii.a2b_hex(datain)
+
+with open(filename, 'wb') as out_file:
+    out_file.write(dataout)
---
+++ streamer/sendfile.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import requests
+import json
+import pprint
+import binascii
+import sys
+
+# config area
+mm_ip = '127.0.0.1'
+mm_port = '7782'
+# end of config area
+
+#Define marketmaker URL
+mm_url = 'http://' + mm_ip + ':' + mm_port
+userpass = "1d8b27b21efabcd96571cd56f91a40fb9aa4cc623d273c63bf9223dc6f8cd81f"
+
+# configure pretty printer
+pp = pprint.PrettyPrinter(width=41, compact=True)
+
+# define function that posts json data to marketmaker
+def post_rpc(url,payload):
+    try:
+        r = requests.post(url, data=json.dumps(payload))
+        return(json.loads(r.text))
+    except Exception as e:
+        print("Couldn't connect to " + url, e)
+        sys.exit(0)
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    print('please spscify path to file')
+    sys.exit()
+
+with open(filename, 'rb') as in_file:
+    datain = in_file.read()
+
+# convert file to hex string
+dataout = binascii.hexlify(datain)
+
+#Define streamerqadd API JSON
+queueadd = {
+	"userpass" : userpass,
+	"method" : "streamerqadd"
+    "data" : dataout
+    "seqid" : 1
+}
+
+#Send payload withdraw API
+response = post_rpc(mm_url,withdraw)
+print('== response ==')
+pp.pprint(response)
