fe3e11613571e19f15b85adf7788c906930468d3
blackjok3r <blackjok3r@gmail.com>
2019-04-27 13:23:59 +0800

    fix

--- CC_scripts/migrations_python/migration_script_v4.py
+++ CC_scripts/migrations_python/migration_script_v4.py
@@ -371,7 +371,6 @@ if len(exports_filename) == 0:
         export_obj['src_txid'] = sent_tx
         export_obj['payouts'] = payouts
         export_obj['src_hex'] = signed_hex
-        export_obj['size'] = export_ret['size']
         with open(exports_filename, "a+") as export_transactions_file:
             export_transactions_file.write("%s\n" % json.dumps(export_obj))
         t = threading.Thread(target=do_migrate, args=(src_chain, dest_chain, sent_tx, payouts, signed_hex, index))
